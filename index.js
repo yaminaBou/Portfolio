2384aaadf931ffc3d11eb5a17309eabc1e49dc66
yaminaBou <yboutchich82@gmail.com>
2024-12-05 14:53:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const form = document.querySelector('#contact-form');
-const nom = document.querySelector('#nom');
-const email = document.querySelector('#email');
-const message = document.querySelector('#message');
 
-form.addEventListener('submit', (event) => {
-  if (!nom.value || !email.value || !message.value) {
-    event.preventDefault();
-    alert('Veuillez remplir tous les champs du formulaire.');
-  }
-});
